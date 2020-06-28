606131c0640a7ca53bf4ce0f8154199d0bfccb90
nmcanall <nmcanall@citadel.edu>
2020-06-28 12:16:38 -0700

    display current data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,36 +5,67 @@ var currentCityHeaderEl = document.querySelector("#current-city-header");
 var getWeatherData = function(city) {
     var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=7fc21c7de7016c8d72a7a8f065f6d9c4";
 
-    fetch(apiURL).then(function(response) {
+    // Use first fetch to get latitude and longitude of user-input city
+    fetch(apiURL).then(function(forecastResponse) {
+        return forecastResponse.json(); 
+    })
+    .then(function(forecastData) {
+        var lat = forecastData.city.coord.lat;
+        var lon = forecastData.city.coord.lon;
 
-        // Check if good URL
-        if(response.ok) {
-            response.json().then(function(data) {
-                printCurrentData(data);
-                printFiveDay(data);
-            });
-        }
+        // Once we get lat/long, use second fetch to retrieve data from OneCall API
+        var onecallURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=7fc21c7de7016c8d72a7a8f065f6d9c4"
+        return fetch(onecallURL)
+    })
+    .then(function(response) {
+        return response.json();
+    })
 
-        // Display error if the URL is bad (city not found)
-        else {
-            alert("Error: " + response.statusText);
-        }
+    // Function that provides functionality to build page
+    .then(function(data) {
+        printCurrentData(data, city);
     })
 
     // Error if bad connection to server
-    .catch(function(error) {
-        alert("Unable to connect to OpenWeather");
-    });
+    // .catch(function(error) {
+    //     alert("Unable to connect to OpenWeather");
+    // });
 };
 
-var printCurrentData = function(data) {
+var printCurrentData = function(data, city) {
 
     // Update the current city data header
-    var city = data.city.name;
-    var iconImgEl = getIcon(data.list[0].weather[0].icon, data.list[0].weather[0].description);
-    var date = convertDate(data.list[0].dt_txt);
+    var iconImgEl = getIcon(data.current.weather[0].icon, data.current.weather[0].description);
+    var date = convertDate(data.current.dt);
     currentCityHeaderEl.textContent = city + " (" + date + ") ";
     currentCityHeaderEl.appendChild(iconImgEl);
+
+    // Get current stats
+    var temp = data.current.temp;
+    var humidity = data.current.humidity;
+    var windSpeed = data.current.wind_speed;
+    var uvIndex = data.current.uvi;
+
+    // Update current stats on screen (span elements available in HTML)
+    document.querySelector("#temperature").textContent = temp;
+    document.querySelector("#humidity").textContent = humidity;
+    document.querySelector("#wind-speed").textContent = windSpeed;
+    var uvIndexEl = document.querySelector("#uv-index");
+
+    // Set data for UV Index
+    uvIndexEl.textContent = uvIndex;
+    if(uvIndex > 11) {
+        uvIndexEl.classList = "bg-dark text-white p-2";
+    }
+    else if(uvIndex > 8) {
+        uvIndexEl.classList = "bg-danger text-white p-2";
+    }
+    else if(uvIndex > 4) {
+        uvIndexEl.classList = "bg-warning p-2";
+    }
+    else {
+        uvIndexEl.classList = "bg-success text-white p-2";
+    }
 };
 
 var printFiveDay = function(data) {
@@ -49,11 +80,9 @@ var getIcon = function(iconCode, description) {
     return iconImg;
 }
 
-// Helper method to format date without adding moment.js
+// Helper method to format date
 var convertDate = function(longDate) {
-    var shortDate = longDate.split(" ")
-    var dateArray = shortDate[0].split("-");
-    return (dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0]);
+    return longDate;
 }
 
 getWeatherData("Laguna Hills");
